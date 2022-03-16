c037a6d2fabf1d3f073d1573c962ea3afec65942
Mariaf1714 <maria_frik@web.de>
2022-03-16 18:54:39 +0100

    debugging

--- starter/main.py
+++ starter/main.py
@@ -9,10 +9,12 @@ from pydantic import BaseModel, Field
 import os
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    print("Running DVC")
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
+        exit("Pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 
 
 model_pth = os.path.join(os.path.dirname(
